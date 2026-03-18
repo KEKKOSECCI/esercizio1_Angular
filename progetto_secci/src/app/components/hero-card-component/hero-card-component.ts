95d0c7e44d65a66482a236604b8c09af8d8f6eae
scuola_secci <you@example.com>
2026-03-18 13:48:48 +0100

    Secondo Commit

---
+++ progetto_secci/src/app/components/hero-card-component/hero-card-component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-card-component',
+  imports: [CommonModule],
+  templateUrl: './hero-card-component.html',
+  styleUrl: './hero-card-component.css',
+})
+export class HeroCardComponent {
+
+}
